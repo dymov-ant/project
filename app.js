2a83e0175510e765f6e9f02c876b7981473edf82
Anton Dymov <dymov.ant@gmail.com>
2021-01-04 19:54:04 +0500

    add redirect in production

--- app.js
+++ app.js
@@ -26,7 +26,10 @@ async function start() {
 }
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client", "build")));
+    app.use("/", express.static(path.join(__dirname, "client", "build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
 start();
