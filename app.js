d5956751617e52221af4d7d4de3389eb44a01416
Anton Dymov <dymov.ant@gmail.com>
2021-01-04 18:59:39 +0500

    deploy heroku

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "content")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(express.json({extended: true}));
 
 app.use("/api/v1/auth", require("./routes/auth.routes"));
@@ -26,5 +27,6 @@ async function start() {
 }
 start();
 
-const PORT = config.get("PORT") || 5000;
+// const PORT = config.get("PORT") || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
